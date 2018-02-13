5a3cac2ac41573ce29b37e6cda8e6cfed2217e76
Kadi Kraman <kadi.kraman@formidable.com>
2018-02-13 16:33:12 +0000

    Add intro slides

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -1,70 +1,105 @@
 // Import React
-import React from "react";
+import React from 'react';
+import deepmerge from 'deepmerge';
+import createTheme from 'spectacle/lib/themes/default';
+import createCustomTheme from 'spectacle-theme-nova';
+import { Deck, Heading, Slide, Text, CodePane, Image } from 'spectacle';
+import orangeSrc from '../assets/orange.jpg';
 
-// Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text
-} from "spectacle";
-
-// Import theme
-import createTheme from "spectacle/lib/themes/default";
-
-// Require CSS
-require("normalize.css");
-
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = deepmerge(createTheme(), createCustomTheme());
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck transition={['zoom']} transitionDuration={500} theme={theme}>
+        <Slide>
+          <Heading size={1} fit lineHeight={1} textColor="heading">
+            Getting started with Apollo GraphQL
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            an optinionated end to end journey with
+          </Text>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            with Node.js and React
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide>
+          <Heading size={1} fit lineHeight={1} textColor="heading">
+            Getting started with GraphQL
+          </Heading>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+        <Slide>
+          <Heading size={2} textColor="heading">
+            Apollo
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            Industry leader in GraphQL tooling
+          </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        <Slide>
+          <Heading size={2} textColor="heading">
+            Node.js
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            Everyone who writes frontend code will be able to use it
+          </Text>
+        </Slide>
+        <Slide>
+          <Heading size={2}>React</Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            Arguably the best UI framework
+          </Text>
+        </Slide>
+        <Slide>
+          <Heading size={2}>End to end</Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            See how ALL the pieces fit together
+          </Text>
+        </Slide>
+        <Slide>
+          <Heading size={2}>Optinionated</Heading>
+          <Text margin="10px 0 40px 0" textColor="tertiary" size={1} bold>
+            There's many ways to peel an orange. Here's how I would do it.
+          </Text>
+          <Image src={orangeSrc} />
+        </Slide>
+        <Slide>
+          <Heading size={2}>GraphQL</Heading>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <CodePane
+              style={{
+                padding: '20px',
+                width: '33%',
+                display: 'inline-block',
+                minWidth: '33%',
+              }}
+              margin="20px auto"
+              lang="graphql"
+              source={require('raw-loader!../assets/graphqlExampleTypes.example')}
+            />
+            <CodePane
+              style={{
+                padding: '20px',
+                width: '33%',
+                display: 'inline-block',
+                minWidth: '33%',
+              }}
+              margin="20px auto"
+              lang="graphql"
+              source={require('raw-loader!../assets/graphqlExample.example')}
+            />
+            <CodePane
+              style={{
+                padding: '20px',
+                width: '33%',
+                display: 'inline-block',
+                minWidth: '33%',
+              }}
+              margin="20px auto"
+              lang="json"
+              source={require('raw-loader!../assets/graphqlExampleResult.example')}
+            />
+          </div>
         </Slide>
       </Deck>
     );
